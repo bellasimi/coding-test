e4a176f9e3ad5068a2f372a8ffd8f26036aa7a50
bellasimi <WorkSY1111@gmail.com>
2021-10-23 22:38:14 +0900

    폰번호 가리기

--- src/main/java/Tel4.java
+++ src/main/java/Tel4.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Tel4 {
+
 
     public static void main(String[] args) {
         int answer = 0;
         long needs=0;
         long budget =10;
-        int [] d = {1,2,3,4};
-        Arrays.sort(d);
+        int [] d={1,2,3,4};
+
         for(int i=0;i<d.length;i++){
             needs +=d[i];
 
---
+++ src/main/java/Telmozaic5.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Telmozaic5 {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("폰번호를 입력하시오");
+        String s = sc.next();
+
+        int leng = s.length();
+
+        System.out.println(leng);
+
+        char[] answer = s.toCharArray();
+
+        for(int i=0;i<leng-4;i++){
+            answer[i] = '*';
+        }
+        System.out.println(String.valueOf(answer));
+
+        /* 이방법은 너무 느림
+        String star="";
+        String replace = s.substring(0,leng-4);
+        for(int i=0;i<replace.length();i++){
+            star+="*";
+        }
+        replace.replace(replace,star);
+
+        System.out.println(star+s.substring(leng-4,leng));
+        */
+
+
+        /* 이건 자바 11부터 가능
+        String change = s.substring(0,leng-4);
+        String star = "*";
+        System.out.println(s.replace(change,star.repeat(change.length())));//이건 자바11부터 가능*/
+
+    }
+}

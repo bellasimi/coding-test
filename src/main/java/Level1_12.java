858b389631d65daec67b9d7677a941b30cf65310
bellasimi <WorkSY1111@gmail.com>
2021-10-26 23:58:04 +0900

    제곱근 판별

---
+++ src/main/java/Level1_12.java
@@ -0,0 +1,15 @@
+public class Level1_12 {
+
+    public static void main(String[]args){
+        long n = 5;
+        int x =2;
+
+        if(x*x==n){
+            System.out.println((x+1)*(x+1));
+        }
+        else{
+            System.out.println(-1);
+        }
+
+    }
+}

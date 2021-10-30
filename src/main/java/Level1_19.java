b6a3afc76f5bfb3c72a7325ae20a6b76d34b1b40
bellasimi <WorkSY1111@gmail.com>
2021-10-30 22:52:51 +0900

    문자열 정수로 바꾸기

---
+++ src/main/java/Level1_19.java
@@ -0,0 +1,27 @@
+public class Level1_19 {
+    /*https://programmers.co.kr/learn/courses/30/lessons/12925/solution_groups?language=java*/
+    public static void main(String[]args){
+        String s = "-1234";
+        String answer ="";
+        Boolean con=false;
+
+        /* 한줄 풀이
+        int result= s.matches("\\+?[1-9][0-9]{0,4}")? Integer.parseInt(s): Integer.parseInt(s.replace("-",""))*-1;*/
+        int result=0;
+
+        for(char c:s.toCharArray()){
+            if(c=='-'){
+                con=false;
+            }
+            else if(c!='+'){
+                con=true;
+            }
+        }
+
+        result = con==false? Integer.parseInt(s)*-1: Integer.parseInt(s);
+
+        System.out.println(answer);
+        System.out.println(result);
+    }
+
+}

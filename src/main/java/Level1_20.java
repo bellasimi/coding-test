c3edd7df3f4c43c635de6a2adfd46cc4b97d783b
bellasimi <WorkSY1111@gmail.com>
2021-10-31 16:43:34 +0900

    수박문자열 찍기

---
+++ src/main/java/Level1_20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Level1_20 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수 n입력");
+        int n = sc.nextInt();
+        String answer="";
+
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                answer+="수";
+            }
+            else{
+                answer+="박";
+            }
+
+        }
+        /*for(int i=0;i<n;i++){
+            answer += i%2==0? "수":"박";
+        }*/
+        System.out.println(answer);
+    }
+}

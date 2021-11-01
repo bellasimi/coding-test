b646dbd0ca046511381d307b85c7a18b02415e6b
bellasimi <WorkSY1111@gmail.com>
2021-11-02 00:41:29 +0900

    김서방 찾기 오답: 객체비교 .equals()

--- src/main/java/Level1_22.java
+++ src/main/java/Level1_22.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 public class Level1_22 {
     public static void main(String[]args){
         String[] seoul ={"Jane","Kim"};
+        int index=Arrays.asList(seoul).indexOf("Kim");
+
+        String answer ="김서방은 "+index+"에 있다";
+/*
         String answer ="";
         for(int i=0;i<seoul.length;i++){
-            if(seoul[i]=="Kim"){
+            if(seoul[i].equals("Kim")){
                 answer +="김서방은 "+i+"에 있다.";
                 break;
             }
-        }
+        }*/
         System.out.println(answer);
     }
 }

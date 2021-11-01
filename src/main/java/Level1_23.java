7bdcf472bf480db64de8f29199cd411aae2f2419
bellasimi <WorkSY1111@gmail.com>
2021-11-02 00:42:29 +0900

    문자열 다루기 기본 오답정리: 정규표현식 수정

--- src/main/java/Level1_23.java
+++ src/main/java/Level1_23.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Level1_23 {
@@ -7,12 +8,14 @@ public class Level1_23 {
         String s = sc.next();
         boolean answer=false;
 
-        if(s.matches("[a-z]{1,8}")){
-               answer=false;
-        }else{
-            answer=true;
-        }
-        System.out.println(answer);
+
+        /* length, split() 사용 */
+        answer = (s.length()==4||s.length()==6)&&(s.split("[0-9]").length==0)? true:false;
+        System.out.println(Arrays.toString(s.split("[0-9]")));//만약 전부 숫자로만 이뤄져 있다면, 이 배열의 길이는 0임
+        /* 정규식 풀이
+        answer = s.matches("^[0-9]${4}|{6}")? true:false;
+        */
+
         /*85점
                 try{
             Integer.parseInt(s);
@@ -22,6 +25,6 @@ public class Level1_23 {
             answer=false;
             e.printStackTrace();
         }*/
-
+        System.out.println(answer);
     }
 }

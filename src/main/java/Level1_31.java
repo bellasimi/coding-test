75805ac965eaa231cb71d859049477531e59db26
bellasimi <WorkSY1111@gmail.com>
2021-11-05 20:28:23 +0900

    [LEVEL1]가운데 글자 가져오기 좀 더 나은 풀이

--- src/main/java/Level1_31.java
+++ src/main/java/Level1_31.java
@@ -1,7 +1,16 @@
 public class Level1_31 {
     public static void main(String[]args){
-        String s = "abcde";
+        String s = "abcd";
         String answer = "";
+        int begin = (s.length()-1)/2;
+        int end = s.length()/2+1;
+        System.out.println(begin);
+        System.out.println(end);
+        if(s!=null){
+            answer = s.substring(begin,end);
+        }
+
+        /*
         int centerBegin= (int)Math.floor(s.length()/2.0);
         //s.length()의 반환값이 int이므로 그냥 2로 나누면 항상 정수형으로 값이 나와서. 올림 내림 값이 둘다 같다. 그러니 double로 만들어주자!
         int centerEnd= (int)Math.ceil(s.length()/2.0);
@@ -14,7 +23,7 @@ public class Level1_31 {
             answer += s.substring(centerBegin,centerEnd);
             System.out.println(s.substring(centerBegin,centerEnd));
         }
-
+*/
         System.out.println(answer);
     }
 }

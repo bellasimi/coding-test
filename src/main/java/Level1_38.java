e212867362ffc96609ac7a30980a4e7cd6fd8bc2
bellasimi <WorkSY1111@gmail.com>
2021-11-09 23:02:46 +0900

    [LEVEL1]3진법 뒤집기 오답노트

--- src/main/java/Level1_38.java
+++ src/main/java/Level1_38.java
@@ -1,12 +1,48 @@
-import java.util.Arrays;
-
 public class Level1_38 {
+    /*https://programmers.co.kr/learn/courses/30/lessons/68935/solution_groups?language=java*/
     public static void main(String[]args){
         long n = 45;
         long na = 0;
-        long mok= 1;
-        String threeReverse="";
+        long mok = 1;
+        /*문자열을 해당 진법으로 parseInt()*/
+        long s = System.nanoTime();
+        String answer="";
+        while(n>=1){
+            na = n%3;
+            n/=3;
+            answer += na;
 
+        }
+        int result = Integer.parseInt(answer,3);
+
+        long e = System.nanoTime();
+
+        System.out.println(answer);
+        System.out.println(result);
+        System.out.println(e-s);//41200
+
+
+        /* List<Integer> 에 담기
+        long s = System.nanoTime();
+        List<Long> three = new ArrayList<Long>();
+        while(n>=1){
+            na = n%3;
+            n/=3;
+            three.add(na);
+        }
+        long answer =0;
+        int cnt=three.size()-1;
+        for(long i: three){
+            answer += i*Math.pow(3,cnt);
+            cnt--;
+        }
+
+        long e = System.nanoTime();
+        System.out.println(answer);
+        System.out.println(e-s);//377400
+*/
+/* 60점
+        String threeReverse="";
         while(mok>0){
             mok = n/3;
             na = n%3;
@@ -26,5 +62,7 @@ public class Level1_38 {
         }
         System.out.println(threeReverse);
         System.out.println(answer);
+        */
+
     }
 }

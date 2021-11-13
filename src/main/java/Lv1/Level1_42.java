9ce309e7417b68c2581b506bd70b803288f9d3e6
bellasimi <WorkSY1111@gmail.com>
2021-11-13 21:21:09 +0900

    [LEVEL1] 체육복

--- src/main/java/Lv1/Level1_42.java
+++ src/main/java/Lv1/Level1_42.java
@@ -1,5 +1,5 @@
 package Lv1;
-
+/*https://programmers.co.kr/learn/courses/30/lessons/42862/solution_groups?language=java*/
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -8,44 +8,46 @@ public class Level1_42 {
         int n =3;
         int[] lost = {1,2};
         int[] reserve = {2,3};
-        List<Integer> reserveList = Arrays.stream(reserve).boxed().collect(Collectors.toList());
-        List<Integer> lostList = Arrays.stream(lost).boxed().collect(Collectors.toList());
-        int answer = n - lost.length;
 
-        Map<Integer,Integer> borrow = new HashMap<Integer,Integer>();
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
-        int index = 0;
-        int indexJ = 0;
-        for(int i:reserve){
-            for(int j:lost){
-                if(i==j){
+        int answer = n-lost.length;
+        boolean[] checked = new boolean[n+1];//0~3
+
+        /* 여분인자 확인 */
+
+        for(int l=0; l<lost.length;l++){
+            for(int r =0; r<reserve.length;r++){
+                if(reserve[r] == lost[l]){
+                    checked[reserve[r]] = true;//2,3
                     answer++;
-                    reserveList.remove(index);
-                    lostList.remove(indexJ);
                 }
-                indexJ++;
             }
-            index++;
         }
 
-        System.out.println("reserveList: "+ reserveList);
-        System.out.println("lostList: "+ lostList);
 
-        for(int i:reserveList){
-            for(int j:lostList){
-                if(j==i+1||j==i-1){
-                    if(reserve.length>lost.length){
-                        borrow.put(j,i);
-                    }else{
-                        borrow.put(i,j);
+        /* 여분인자만 체육복 대여 */
+
+        for(int l=0; l<lost.length;l++){
+            if(!checked[lost[l]]){
+                for(int r =0; r<reserve.length;r++){
+                    if(!checked[reserve[r]]){
+                        if(reserve[r]==lost[l]-1){
+                            answer++;
+                            checked[reserve[r]]=true;
+                            break;
+                        }else if(reserve[r]==lost[l]+1){
+                            answer++;
+                            checked[reserve[r]]=true;
+                            break;
+                        }else if(reserve[r]>lost[l]+1){
+                            break;
+                        }
                     }
                 }
-                System.out.println("borrow: "+borrow);
             }
-
         }
-
-        answer = answer + borrow.size();
         System.out.println(answer);
     }
 }

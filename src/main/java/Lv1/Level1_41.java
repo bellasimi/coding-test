38acf979348ab1a64c72cdd86d1d28b594b26e17
bellasimi <WorkSY1111@gmail.com>
2021-11-13 19:35:10 +0900

    [LEVEL1] 포켓몬 Set방식

--- src/main/java/Lv1/Level1_41.java
+++ src/main/java/Lv1/Level1_41.java
@@ -1,31 +1,24 @@
 package Lv1;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Level1_41 {
     public static void main(String[]args){
         int[] nums = {1,2,3,4,5,6};
         int answer = 0;
 
-        Arrays.sort(nums);
-
-        /*List<Integer> type = new ArrayList<Integer>();
-        type.add(nums[0]);
-        List getLast() 함수가 없네? 마지막 인자 계속 비교해야 되는데 못쓰것네...
-        */
-
-        String type = String.valueOf(nums[0]);
+        /*Set을 이용하는 방법*/
+        Set<Integer> type = new HashSet<Integer>();
 
         for(int i:nums){
-            if(!type.contains(String.valueOf(i))){
-                type+=i;
-            }
+            type.add(i);
         }
 
-        System.out.println(type);
 
         int get = nums.length/2;
-        int typeTotal = type.length();
+        int typeTotal = type.size();
 
         answer = get>typeTotal? typeTotal:get;
 

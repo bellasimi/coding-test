94cac91c0aeacd2144e7344d56f868eaadc4541f
bellasimi <WorkSY1111@gmail.com>
2021-11-16 00:51:39 +0900

    [LEVEL1]완주하지 못한 선수 List 사용 효율성 탈락

--- src/main/java/Lv1/Level1_45.java
+++ src/main/java/Lv1/Level1_45.java
@@ -3,30 +3,31 @@ package Lv1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Level1_45 {
     public static void main(String[]args){
-        //String[] participant = {"mislav","mislav","stanko","ana"};
-        //String[] completion = {"stanko","ana","mislav"};
-        //List<String> loser = new ArrayList<String>(Arrays.asList(participant));
-
-        //List<String> winner = new ArrayList<String>(Arrays.asList(completion));
-        String[] participant = {"mislav","mislav","stanko","ana"};
+        String[] participant = {"mislav","stanko","ana","mislav"};
         String[] completion = {"stanko","ana","mislav"};
-        List<String> winner = Arrays.asList(completion);
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        List<String> player = new ArrayList<String>(Arrays.asList(participant));
+        String answer="";
+        List<String> winner = new ArrayList<String>(Arrays.asList(completion));
 
 
-        for(String c:completion){
-            for(String p:participant){
-                if(p==c){
-                    winner.remove(p);
-                }
+        for(String p:participant){
+            if(winner.contains(p)){
+               player.remove(p);
+               winner.remove(p);
             }
-            System.out.println(winner);
         }
+        answer = player.get(0);
 
-
-       //System.out.println(loser);
+        System.out.println(player);
+        System.out.println(answer);
     }
 
 }

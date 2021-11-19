2c31181ce9b41e2a8627b27e3637cad35404d1a7
bellasimi <WorkSY1111@gmail.com>
2021-11-19 23:40:27 +0900

    [LEVEL1]긴 숫자에선 case문이 배열 2개보다 유리함

--- src/main/java/Lv1/Level1_51.java
+++ src/main/java/Lv1/Level1_51.java
@@ -10,7 +10,10 @@ public class Level1_51 {
 
     public long solution(String s) {
         long answer = 0;
-        String[] nums = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] nums2 = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        /* switch문 쓰는 방법 0.35ms*/
 
         for(String n:nums){
             if(s.contains(n)){
@@ -18,46 +21,55 @@ public class Level1_51 {
             }
         }
         answer = Integer.parseInt(s);
+
         return answer;
+
+        /* 두개의 같은 길이 배열로 풀이 0.44ms
+        for (int i= 0; i < nums.length ; i++) {
+            if(s.contains(nums[i])){
+                s = s.replaceAll(nums[i],nums2[i]);
+            }
+        }
+        answer = Integer.parseInt(s);*/
+
     }
 
-    public String change(String n){
-        String change ="";
-        switch (n){
-            case "zero" :
+
+    public String change(String n) {
+        String change = "";
+        switch (n) {
+            case "zero":
                 change = "0";
                 break;
-            case "one" :
+            case "one":
                 change = "1";
                 break;
-            case "two" :
+            case "two":
                 change = "2";
                 break;
-            case "three" :
+            case "three":
                 change = "3";
                 break;
-            case "four" :
+            case "four":
                 change = "4";
                 break;
-            case "five" :
+            case "five":
                 change = "5";
                 break;
-            case "six" :
+            case "six":
                 change = "6";
                 break;
-            case "seven" :
+            case "seven":
                 change = "7";
                 break;
-            case "eight" :
+            case "eight":
                 change = "8";
                 break;
-            case "nine" :
+            case "nine":
                 change = "9";
                 break;
 
         }
         return change;
     }
-
-
 }

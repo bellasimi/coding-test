eb76ee5c40e4cc3c9326d41b540285ca7e8e86bd
bellasimi <WorkSY1111@gmail.com>
2021-11-21 16:52:38 +0900

    [LEVEL2]JadenCase 문자열 만들기 오답 43점

--- src/main/java/Lv2/Level2_2.java
+++ src/main/java/Lv2/Level2_2.java
@@ -1,48 +1,63 @@
 package Lv2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Locale;
 
 public class Level2_2 {
-
-    public static void main(String[]args){
-        int[][] arr1 = {{2,3,2},{4,2,4},{3,1,4}};
-        int[][] arr2 = {{5,4,3},{2,4,1},{3,1,1}};
+    public static void main(String[] args){
+        String s = "3people unFollowed me";
         Level2_2 l = new Level2_2();
-        System.out.println(l.solution(arr1,arr2));
+        System.out.println(l.solution(s));
     }
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int len1 = Math.max(arr1.length, arr2.length);
-        int len2 = arr1[0].length;
-        int[][] answer = new int[len1][len2];
-        List<Integer> result = new ArrayList<Integer>();
-
-       int[] arr = new int[len2];
-       for(int[] a1:arr1){
-            for(int[] a2:arr2){
-                    int a = 0;
-                    for(int i=0;i<len2;i++){
-                        a += a1[i]*a2[i];
-                        System.out.println(Arrays.toString(a1));
-                        System.out.println(Arrays.toString(a2));
-                        System.out.println(a1[i]+"x"+a2[i]+"="+a1[i]*a2[i]);
-                    }
-                    result.add(a);
+    public String solution(String s) {
+        String answer = "";
+        int index=0;
+        /*s를 전부 돌면서 " "나올 때마다 index 0으로 만들기 4ms 18점*/
+        s = s.toLowerCase();
+        System.out.println(s);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                index =0;
             }
-
+            System.out.println(index);
+            if(i==0){
+                if(!checkNum(s.substring(0,1))) {
+                    s = s.replaceFirst(s.substring(0,1),s.substring(0,1).toUpperCase());
+                }
+            }
+            else if(i!=0&&index==0){
+                if(!checkNum(s.substring(i + 1, i + 2))) {
+                    s = s.replaceFirst(s.substring(i + 1, i + 2), s.substring(i + 1, i + 2).toUpperCase());
+                }
+            }
+            index++;
         }
 
-       for(int i=0;i<len1;i++){
-           for(int j=0;j<len2;j++){
-
-           }
-       }
-
-        System.out.println(result);
-        System.out.println(Arrays.toString(answer));
+        answer = s;
         return answer;
+
+
+        /* split()-> +=로 붙이기 잘못된 풀이 13ms 43점
+        s = s.toLowerCase();
+        String[] arrS = s.split(" ");
+        for(int i=0;i< arrS.length;i++){
+            if(!checkNum(arrS[i])){
+                arrS[i] = arrS[i].replaceFirst(arrS[i].substring(0,1),arrS[i].substring(0,1).toUpperCase());
+
+            }
+            System.out.println(arrS[i]);
+            if(i==0){
+                answer += arrS[i];
+            }
+            else{
+                answer += " "+ arrS[i];
+            }
+        }
+        */
     }
 
+    public boolean checkNum(String a){
+        boolean flag = a.substring(0,1).matches("[0-9]");
+        return flag;
+    }
 }
 

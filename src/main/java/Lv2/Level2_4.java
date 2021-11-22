12a1c402e39e1efccb48ccb1204e95c98687e61c
bellasimi <WorkSY1111@gmail.com>
2021-11-22 18:47:38 +0900

    [LEVEL2]피보나치의 수 배열 조건문 사용 4ms 정답

--- src/main/java/Lv2/Level2_4.java
+++ src/main/java/Lv2/Level2_4.java
@@ -9,11 +9,29 @@ public class Level2_4 {
         Level2_4 l = new Level2_4();
         System.out.println(l.solution(n));
     }
-    /* (A+B)%C = ((A%C)+(A%C))%C*/
+    /* (A+B)%C = ((A%C)+(A%C))%C*/ //안그러면 int 초과
+    /* 배열, 조건문 사용 100점 4ms*/
     public int solution(int n) {
-        int answer = 0;
-
-        return answer;
+        int result = 0;
+        int[] value = new int[n + 1];
+        value[0] = 0;
+        value[1] = 1;
+        value[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            int temp = value[i - 1] + value[i - 2];
+            if (temp < 1234567) { //1234567미만의 수는 나머지가 그 수임
+                value[i] = temp;
+                if (i == n) {
+                    result = value[n];//이러면 나중에 반복문 또 돌 필요 없음
+                }
+            } else {
+                value[i] = ((value[i - 1] % 1234567) + (value[i - 2] % 1234567))% 1234567;
+                if (i == n) {
+                    result = value[n];
+                }
+            }
+        }
+        return result;
         /* 배열 사용 43점 3ms 오답
         int[] value = new int[n+1];
         value[0] = 0;
@@ -47,5 +65,7 @@ public class Level2_4 {
         }
         long answer = map.get(n)%1234567;
         return answer;*/
+
     }
+
 }

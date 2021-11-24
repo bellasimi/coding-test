babe8b24c8e703d93296b657809af29b8a5cd141
bellasimi <WorkSY1111@gmail.com>
2021-11-24 17:15:46 +0900

    [LEVEL2]숫자의 표현 100점

--- src/main/java/Lv2/Level2_7.java
+++ src/main/java/Lv2/Level2_7.java
@@ -7,11 +7,27 @@ public class Level2_7 {
         System.out.println(l.solution(n));
     }
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
+        int start = 1;
+        while(start<=n){
+            int sum =0;
+            for(int i=start;i<=n;i++){
+                sum += i;
+                if(sum==n){
+                    answer++;
+                    break;
+                }
+                else if(sum>n){//이걸 안해주면 반복문을 더 돔!!
+                    break;
+                }
+            }
+            start++;
+        }
 
 
 /*      등차수열 활용 70점 효율성에서 30감점
 
+        int answer = 1;
         for(int i = 1; i<n; i++){
             for(int j = i+1; j<n;j++){
                 if((j-i+1)*(j+i)==2*n){

1b475e24a4c2c80f5d1518ae2733392fde14b5e8
bellasimi <WorkSY1111@gmail.com>
2021-11-25 20:41:28 +0900

    [LEVEL2] 다음 큰 숫자 효율적 정답

--- src/main/java/Lv2/Level2_9.java
+++ src/main/java/Lv2/Level2_9.java
@@ -7,8 +7,27 @@ public class Level2_9 {
         System.out.println(l.solution(n));
     }
     public int solution(int n) {
-        int cntOne = 0;
-       /*쉽게 2진법 전환 */
+
+        /* oneCnt(int n) 함수 따로 뺴서 1 cnt 325400ns*/
+        //함수를 따로 빼는게 효육적이군
+        int cnt = oneCnt(n);
+        int answer = n+1;
+        while(true){
+
+            if(cnt==oneCnt(answer)){
+                break;
+            }
+
+            answer++;
+        }
+
+        return answer;
+        
+        /*while문에서 1 직접 cnt  시간초과 510500ns*//*
+
+
+         *//*쉽게 2진법 전환 *//*
+
         String bi = Integer.toBinaryString(n);
         System.out.println(Integer.toBinaryString(n));
 
@@ -44,6 +63,7 @@ public class Level2_9 {
             answer++;
         }
 
+*/
 
 /*      무식하게 2진법 바꾸기
 
@@ -56,6 +76,17 @@ public class Level2_9 {
         }
         bi = bi.reverse();
         System.out.println(bi);*/
-        return answer;
+    }
+
+    public int oneCnt(int n){
+        String bi = Integer.toBinaryString(n);
+        char[] arrBi2 = bi.toCharArray();
+        int cntOne = 0;
+        for(char c: arrBi2){
+            if(c =='1'){
+                cntOne++;
+            }
+        }
+        return cntOne;
     }
 }

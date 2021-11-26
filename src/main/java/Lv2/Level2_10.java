a2ecd85d496b8ab52b3e97db5dc013ea3dd3ffbf
bellasimi <WorkSY1111@gmail.com>
2021-11-26 20:22:08 +0900

    [LEVEL2] 올바른 괄호 92점

--- src/main/java/Lv2/Level2_10.java
+++ src/main/java/Lv2/Level2_10.java
@@ -1,13 +1,39 @@
 package Lv2;
 
+import java.util.Arrays;
+
 public class Level2_10 {
     public static void main(String[]args){
-        String s ="()()";
+        String s ="()))((()";
         Level2_10 l = new Level2_10();
         System.out.println(l.solution(s));
     }
     boolean solution(String s) {
         boolean answer = true;
+        boolean warn = true;
+        long start = System.currentTimeMillis();
+        /*char[] 사용 0.-ms 5,11번 통과가 안돼.. ,92점
+        char[] arrS = s.toCharArray();
+        int sum = 0;
+        if(check2(arrS[0],arrS[arrS.length-1])){
+            for(char c: arrS){
+                sum += c=='('? 1:-1;
+                System.out.println(sum);
+                if(sum<-1){
+                    warn = false;
+                    break;
+                }
+            }
+            answer = sum==0&&warn==true? true:false;
+
+
+        }
+        else{
+            answer = false;
+        }
+
+*/
+/*      String[] 사용 2ms 시간초과, 60점
         String[] arrS = s.split("");
         int sum = 0;
         System.out.println(check(arrS[0],arrS[arrS.length-1]));
@@ -21,10 +47,22 @@ public class Level2_10 {
         else{
             answer = false;
         }
+*/
 
+        long end = System.currentTimeMillis();
+        System.out.println("시간 :"+(end-start)+"ms");
         return answer;
     }
-
+    boolean check2(char c, char c2){
+        boolean result = true;
+        if(c=='('&&c2==')'){
+            result = true;
+        }
+        else{
+            result = false;
+        }
+        return result;
+    }
     boolean check(String s, String s2){
         boolean result = true;
         if(s.equals("(")&&s2.equals(")")){

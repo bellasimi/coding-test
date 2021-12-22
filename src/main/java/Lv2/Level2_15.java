86f1302670138273ad0375ca5c00ed8239b748e6
bellasimi <WorkSY1111@gmail.com>
2021-12-22 15:07:10 +0900

    [LEVEL2] 전화번호 목록 테스통과 but 시간초과

--- src/main/java/Lv2/Level2_15.java
+++ src/main/java/Lv2/Level2_15.java
@@ -1,4 +1,5 @@
 package Lv2;
+/*https://programmers.co.kr/learn/courses/30/lessons/42577*/
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -12,6 +13,32 @@ public class Level2_15 {
 
     public boolean solution(String[] phone_book) {
 
+/*      String len으로 Arrays.sort 이후 substring으로 접두어 판별 -> 테스트 통과 but 효율성 실패  
+        int len = phone_book.length;
+        boolean answer = true;
+
+        Arrays.sort(phone_book, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
+
+        for(int i = 0; i<len-1;i++){
+            for(int j = i+1; j< len;j++){
+                int lenJ = phone_book[j].length();
+
+                if(phone_book[i].substring(0,lenJ).contains(phone_book[j])){
+
+                    answer = false;
+                    break;
+                }
+
+            }
+
+        }
+
+        return answer;*/
  /*    83점 풀이
       int len = phone_book.length;
         boolean answer = true;

37014c04c0ffd98ca899d8bdd436f43d0ae3f23d
bellasimi <WorkSY1111@gmail.com>
2021-12-24 15:35:38 +0900

    [LEVEL2] for문 풀이 36.4

--- src/main/java/Lv2/Level2_16.java
+++ src/main/java/Lv2/Level2_16.java
@@ -1,5 +1,6 @@
 package Lv2;
-
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42586*/
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +14,52 @@ public class Level2_16 {
         System.out.println(l.solution(progresses,speeds));
     }
     public List<Integer> solution(int[] progresses, int[] speeds) {
-        Stack<Integer> stack = new Stack<>();
+        /*36.4점 for문 풀이*/
+
+        List<Integer> list = new ArrayList<>(); // 같은날 배포되는 프로젝트의 수 리스트
+        int[] answer = new int[progresses.length]; //끝나는 기간 배열
+        /* 각각의  프로젝트가 끝나는 기간 배열 구하기 */
+        for(int i = 0; i< progresses.length;i++){
+            int pro = progresses[i];
+            int time = 0;
+            while(true){
+                pro += speeds[i];
+                time++;
+                if(pro>=100){
+                    answer[i] = time;
+                    break;
+
+                }//if
+
+            }//while
+        }//for
+        //System.out.println(Arrays.toString(answer));
+
+        for(int i=0; i< answer.length;i++){
+            int cnt = 0;
+            if(answer[i]<0){
+                continue;
+            }
+            int done = answer[i];
+            for(int j= 0; j< answer.length;j++){
+                //System.out.println("done: "+done+" j: "+answer[j]);
+                if(answer[j]<=done&&answer[j]>0){
+                    answer[j] = -1;
+                    cnt++;
+                }
+
+            }
+            //System.out.println(Arrays.toString(answer));
+            list.add(cnt);
+
+        }
+
+        return list;
+
+
+
+
+/*        Stack<Integer> stack = new Stack<>();
         Stack<Integer> pro = new Stack<>();
         int done = 0;
         int cnt = 0;
@@ -42,6 +88,6 @@ public class Level2_16 {
             }//for
         }//while
 
-        return list;
+        return list;*/
     }
 }

43b7af6d5bb5b70e50ed4fec9ecba851ab51b517
bellasimi <WorkSY1111@gmail.com>
2021-12-26 19:49:22 +0900

    [LEVEL2] 주식가격 큐 테스트 1통과

--- src/main/java/Lv2/Level2_17.java
+++ src/main/java/Lv2/Level2_17.java
@@ -1,6 +1,10 @@
 package Lv2;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /*https://programmers.co.kr/learn/courses/30/lessons/42584*/
 public class Level2_17 {
 
@@ -11,7 +15,21 @@ public class Level2_17 {
     }
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i< prices.length;i++){
+            q.offer(prices[i]);
+            for(int j=i+1;j< prices.length;j++){
+               if(q.peek()<=prices[j]){
+                   q.offer(prices[j]);
+               }
+            }
+            answer[i] = q.size()-1;
+            q.clear();
+        }
+
+        System.out.println(Arrays.toString(answer));
+
+       /* for(int i=0;i< prices.length;i++){
             int cnt = 0;
             for(int j=i+1;j< prices.length;j++){
                 if(prices[i]<=prices[j]){
@@ -20,7 +38,7 @@ public class Level2_17 {
             }
             answer[i] = cnt;
         }
-        //System.out.println(Arrays.toString(answer));
+        //System.out.println(Arrays.toString(answer));*/
         return answer;
     }
 }

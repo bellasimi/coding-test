311906ac843c39f4b63ca010558427da67e8f1dd
bellasimi <WorkSY1111@gmail.com>
2021-12-28 20:33:53 +0900

    [LEVEL2] 124 나라의 숫자 더나은 풀이

--- src/main/java/Lv2/Level2_19.java
+++ src/main/java/Lv2/Level2_19.java
@@ -2,12 +2,12 @@ package Lv2;
 /*https://programmers.co.kr/learn/courses/30/lessons/12899*/
 public class Level2_19 {
     public static void main(String[] args){
-        long n = 9;
+        int n = 9;
         Level2_19 l = new Level2_19();
         System.out.println(l.solution(n));
     }
-    public String solution(long n) {
-        String answer = "";
+    public String solution(int n) {
+/*        String answer = "";
         while(n>0){
             System.out.println(n);
             if(n%3==0){
@@ -17,6 +17,14 @@ public class Level2_19 {
                 answer = Long.toString(n%3) + answer;
             }
             n /=3;
+        }
+        return answer;*/
+        String[] num = {"4","1","2"};
+        String answer ="";
+
+        while(n>0){
+            answer = num[n%3]+ answer;
+            n = (n-1)/3;
         }
         return answer;
     }

e20120e9c1e4730fb30ac03b5243879adac5496b
bellasimi <WorkSY1111@gmail.com>
2021-12-28 20:29:02 +0900

    [LEVEL2] 124 나라의 숫자 100점

--- src/main/java/Lv2/Level2_19.java
+++ src/main/java/Lv2/Level2_19.java
@@ -1,28 +1,24 @@
 package Lv2;
-
+/*https://programmers.co.kr/learn/courses/30/lessons/12899*/
 public class Level2_19 {
     public static void main(String[] args){
-        long n = 3;
+        long n = 9;
         Level2_19 l = new Level2_19();
         System.out.println(l.solution(n));
     }
     public String solution(long n) {
-        StringBuilder answer = new StringBuilder();
-        String result = "";
-        long first = n;
+        String answer = "";
         while(n>0){
-            answer.append(n%3);
-            n/=3;
+            System.out.println(n);
+            if(n%3==0){
+                answer = "4" +answer;
+                n-=3;
+            }else{
+                answer = Long.toString(n%3) + answer;
+            }
+            n /=3;
         }
-        answer.reverse();
-        if(first%3==0){
-            result = answer.substring(0,answer.length()-2)+"4";
-            //System.out.println(result);
-        }else{
-            result = answer.toString();
-        }
-
-        return result;
+        return answer;
     }
 }
 //3-4,6-124,9-124124

997ad092097e7d4069db7fbe04c358734e2ca7d4
bellasimi <WorkSY1111@gmail.com>
2022-01-02 17:36:30 +0900

    [LEVEL2] 더 맵게 우선순위 큐 시간초과

--- src/main/java/Lv2/Level2_21.java
+++ src/main/java/Lv2/Level2_21.java
@@ -7,31 +7,41 @@ import java.util.PriorityQueue;
 public class Level2_21 {
     public static void main(String[] args){
         int[] scoville = {1,2,3,9,10,12};
-        int K = 7;
+        long K = 7;
         Level2_21 l = new Level2_21();
         System.out.println("ㄷㅂ: "+l.solution(scoville, K));
     }
-    public int solution(int[] scoville, int K) {
+    public int solution(int[] scoville, long K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         int answer = 0;
         for(int s: scoville){
             pq.add(s);
         }
-        while(pq.peek()<K){
-
-            for(int i = 1; i< scoville.length;i++){
+        while(answer != pq.size()){
+            for(int i = 1; i < scoville.length;i++){
+                //System.out.println(pq);
+                //System.out.println("peek: "+pq.peek());
                 int sc1 = scoville[i-1];
                 int sc2 = scoville[i];
-
-                if(sc1<K){
+                //System.out.println("sc1: "+sc1+" sc2: "+sc2);
+                int make = sc1 + (sc2*2);
+                //System.out.println("make: "+make);
+                if(pq.peek()<K){
                     pq.poll();
-                    int make = sc1 + (sc2*2);
+                    pq.poll();
                     pq.add(make);
+
+                    answer++;
+                }else{
+                    return answer;
                 }
+
             }
-            answer++;
+
         }
+        answer = pq.peek()<K? -1: answer;
+
 
 
 

096c0c86f5630f3f65122fe8e510394199629c35
bellasimi <WorkSY1111@gmail.com>
2022-03-12 21:03:43 +0900

    [LEVEL1] JS 핸드폰 번호 가리기 (replace+ regex)

--- src/main/JavaScript/Level1/핸드폰 번호 가리기.js
+++ src/main/JavaScript/Level1/핸드폰 번호 가리기.js
@@ -1,6 +1,16 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12948?language=javascript
+
+/* repeat() + slice */
+//평균 0.04ms 얘가 제일 빠름
+const reSlice = (numb) => {
+    let len = numb.length-4; //이건 1부터 시작이라 -5가 아니라 -4임
+    let answer = '*'.repeat(len) + numb.slice(-4);// slice 음수면 뒤에서 부터 -1 -2 이렇게임
+    return answer;
+
+}
+
 /* for문 + substr 풀이 */
-//평균 0.06ms 정도
+//평균 0.06ms
 const forCal = (numb) => {
     let len = numb.length-4
     var answer = '';
@@ -11,15 +21,16 @@ const forCal = (numb) => {
     return answer;
 }
 
-/* repeat() + slice */
-//평균 0.04ms 얘가 더 빠르네
-const reSlice = (numb) => {
-    let len = numb.length-4; //이건 1부터 시작이라 -5가 아니라 -4임
-    let answer = '*'.repeat(len) + numb.slice(-4);// slice 음수면 뒤에서 부터 -1 -2 이렇게임
-    return answer;
 
+/* replace  정규표현식*/
+//평균 0.11ms 제일 느려
+const regCal = (numb) => {
+    let answer = numb.replace(/\d(?=\d{4})/g,'*');
+    return answer;
 }
 
 function solution(phone_number) {
-    return forCal(phone_number);
+    return reSlice(phone_number);
+    //return regCal(phone_number);
+    //return forCal(phone_number);
 }

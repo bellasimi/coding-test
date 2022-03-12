472c68b81382cdc8858d97c494f1f231f5df983f
bellasimi <WorkSY1111@gmail.com>
2022-03-12 20:37:40 +0900

    [LEVEL1] JS 행렬의 덧셈 map 풀이 방식

--- src/main/JavaScript/Level1/행렬의 덧셈.js
+++ src/main/JavaScript/Level1/행렬의 덧셈.js
@@ -1,4 +1,11 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12950?language=javascript
+
+/* map으로 배열 돌며 계산하는 방식 */
+//for문보다 이게 더 느림 최대 4ms 차이남
+const mapCal = (arr1, arr2) =>
+  arr1.map((row, y) =>
+    row.map((e, x) => e + arr2[y][x]));
+
 function solution(arr1, arr2) {
     var answer = [];
     var len = arr1[0].length
@@ -10,4 +17,5 @@ function solution(arr1, arr2) {
         answer.push(temp);
     }
     return answer;
+    //return mapCal(arr1, arr2);
 }

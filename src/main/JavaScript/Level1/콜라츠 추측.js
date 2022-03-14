32ac4e8049d0734a50439c5d3e88ccc00f60fb12
bellasimi <WorkSY1111@gmail.com>
2022-03-14 17:31:51 +0900

    [LEVEL1] JS 콜라츠 추측 삼항연산자 재귀함수

--- src/main/JavaScript/Level1/콜라츠 추측.js
+++ src/main/JavaScript/Level1/콜라츠 추측.js
@@ -6,6 +6,10 @@ const trialCal = (num, answer = 0) => {
     }
     return num === 1? answer:-1;
 }
+/* 평균 0.08ms 삼항 연산자 + 재귀함수  */
+const reCal = (num, answer = 0) => {
+return num === 1? (answer>=500? -1: answer): (num%2===0? solution(num/2,++answer): solution(num*3+1,++answer));
+}
 /* 평균 0.08ms 일반 연산 조건문 */
 const basicCal = (num) => {
     var answer = 0;
@@ -28,5 +32,7 @@ const basicCal = (num) => {
 }
 
 function solution(num) {
-    return basicCal(num);
+    return trialCal(num);
+    //return basicCal(num);
+    //return reCal(num);
 }

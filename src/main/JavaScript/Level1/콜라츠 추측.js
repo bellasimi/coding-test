2f4f4ef7876f11b61c1b7d8d41cba287c4b33948
bellasimi <WorkSY1111@gmail.com>
2022-03-14 17:30:04 +0900

    [LEVEL1] JS 콜라츠 추측 삼항연산자 단순 연산

--- src/main/JavaScript/Level1/콜라츠 추측.js
+++ src/main/JavaScript/Level1/콜라츠 추측.js
@@ -1,3 +1,11 @@
+/* 평균 0.06ms 삼항 연산자 */
+const trialCal = (num, answer = 0) => {
+    while(num !== 1 && answer !== 500){
+      num = num%2===0? num/2: num*3+1;
+      answer++;
+    }
+    return num === 1? answer:-1;
+}
 /* 평균 0.08ms 일반 연산 조건문 */
 const basicCal = (num) => {
     var answer = 0;

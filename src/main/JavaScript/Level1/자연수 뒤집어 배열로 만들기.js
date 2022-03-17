f0a3341824fb32590fb452dead322d19105c69be
bellasimi <WorkSY1111@gmail.com>
2022-03-17 19:15:29 +0900

    [LEVEL1] JS 자연수 뒤집어 배열로 만들기 - reverse, map

--- src/main/JavaScript/Level1/자연수 뒤집어 배열로 만들기.js
+++ src/main/JavaScript/Level1/자연수 뒤집어 배열로 만들기.js
@@ -1,3 +1,7 @@
+/* 0.05ms 문자 split 하고 revese(), map()으로 정수변환*/
+const splitMap = (n) => {
+    return String(n).split('').reverse().map(e => Number(e));
+}
 /* 0.07ms 문자 split 하고 for문 인덱스 거꾸로 push*/
 const splitFor = (n) => {
     var answer = [];
@@ -10,7 +14,8 @@ const splitFor = (n) => {
     return answer;
 }
 
-function solution(n) {
 
-    return splitFor(n);
+function solution(n) {
+    return splitMap(n);
+    //return splitFor(n);
 }
